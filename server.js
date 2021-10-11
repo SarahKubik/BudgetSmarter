963eeeac18e5289cb8b6711ff2b3408c43ff3925
skk <skkdetroit@gmail.com>
2021-10-10 20:57:26 -0400

    update port details

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = 3000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://68.61.230.143/budget";
+
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
